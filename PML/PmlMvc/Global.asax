0d60354ad3e82d45607910530d6c84624c811078
saverio belviso <sbelviso@gmail.com>
2017-11-08 17:05:44 +0100

    inizio

---
+++ PML/PmlMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PmlMvc.MvcApplication" Language="C#" %>
---
+++ PML/PmlMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PmlMvc.Startup))]
+namespace PmlMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
